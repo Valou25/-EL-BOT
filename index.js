4d166f8a8fa046817c7c9310a409a7ccd35e16a9
Valou25 <48156558+Valou25@users.noreply.github.com>
2019-03-02 15:58:32 +0100

    Update index.js

--- index.js
+++ index.js
@@ -14,21 +14,6 @@ bot.on('guildMemberAdd', member => {
         return channel.send('Bienvenue sur le serveur de Alexpgm' + member.displayName)
         console.log(`${member.displayName} à rejoind le serveur.`)
     }).catch(console.error)
-});
-
-
-
-bot.on('message', msg => {
-    if (msg.content === "bonjour"){
-        msg.reply("Heureux de te revoir parmis nous.")
-    }
-    if (msg.content.match(/salut/i)) {
-            msg.reply('Je suis d\'accord avec toi.')
-    }
-    if (msg.content === prefix + "site"){
-        msg.channel.send("https://alexpgm.000webhostapp.com/")
-        console.log("Une personne a demandé pour aller sur ton site.")
-    }
 
 });
 
